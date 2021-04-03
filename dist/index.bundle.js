9ede398d74344f24f508a42be4144556967a63c8
carra1358 <amandinecarreno13@gmail.com>
2021-04-03 19:18:02 +0200

    unlocked pull origin
    commit pour pouvoir mettre à jour localement après ma merge features/structure

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,1 +1,60 @@
-(self.webpackChunkfisheye=self.webpackChunkfisheye||[]).push([[826],{890:(e,t,n)=>{"use strict";n.d(t,{Z:()=>a});var r=n(645),i=n.n(r)()((function(e){return e[1]}));i.push([e.id,"h1 {\n  \n    color: green;\n}",""]);const a=i},645:e=>{"use strict";e.exports=function(e){var t=[];return t.toString=function(){return this.map((function(t){var n=e(t);return t[2]?"@media ".concat(t[2]," {").concat(n,"}"):n})).join("")},t.i=function(e,n,r){"string"==typeof e&&(e=[[null,e,""]]);var i={};if(r)for(var a=0;a<this.length;a++){var o=this[a][0];null!=o&&(i[o]=!0)}for(var c=0;c<e.length;c++){var s=[].concat(e[c]);r&&i[s[0]]||(n&&(s[2]?s[2]="".concat(n," and ").concat(s[2]):s[2]=n),t.push(s))}},t}},379:(e,t,n)=>{"use strict";var r,i=function(){var e={};return function(t){if(void 0===e[t]){var n=document.querySelector(t);if(window.HTMLIFrameElement&&n instanceof window.HTMLIFrameElement)try{n=n.contentDocument.head}catch(e){n=null}e[t]=n}return e[t]}}(),a=[];function o(e){for(var t=-1,n=0;n<a.length;n++)if(a[n].identifier===e){t=n;break}return t}function c(e,t){for(var n={},r=[],i=0;i<e.length;i++){var c=e[i],s=t.base?c[0]+t.base:c[0],l=n[s]||0,u="".concat(s," ").concat(l);n[s]=l+1;var d=o(u),f={css:c[1],media:c[2],sourceMap:c[3]};-1!==d?(a[d].references++,a[d].updater(f)):a.push({identifier:u,updater:v(f,t),references:1}),r.push(u)}return r}function s(e){var t=document.createElement("style"),r=e.attributes||{};if(void 0===r.nonce){var a=n.nc;a&&(r.nonce=a)}if(Object.keys(r).forEach((function(e){t.setAttribute(e,r[e])})),"function"==typeof e.insert)e.insert(t);else{var o=i(e.insert||"head");if(!o)throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");o.appendChild(t)}return t}var l,u=(l=[],function(e,t){return l[e]=t,l.filter(Boolean).join("\n")});function d(e,t,n,r){var i=n?"":r.media?"@media ".concat(r.media," {").concat(r.css,"}"):r.css;if(e.styleSheet)e.styleSheet.cssText=u(t,i);else{var a=document.createTextNode(i),o=e.childNodes;o[t]&&e.removeChild(o[t]),o.length?e.insertBefore(a,o[t]):e.appendChild(a)}}function f(e,t,n){var r=n.css,i=n.media,a=n.sourceMap;if(i?e.setAttribute("media",i):e.removeAttribute("media"),a&&"undefined"!=typeof btoa&&(r+="\n/*# sourceMappingURL=data:application/json;base64,".concat(btoa(unescape(encodeURIComponent(JSON.stringify(a))))," */")),e.styleSheet)e.styleSheet.cssText=r;else{for(;e.firstChild;)e.removeChild(e.firstChild);e.appendChild(document.createTextNode(r))}}var p=null,h=0;function v(e,t){var n,r,i;if(t.singleton){var a=h++;n=p||(p=s(t)),r=d.bind(null,n,a,!1),i=d.bind(null,n,a,!0)}else n=s(t),r=f.bind(null,n,t),i=function(){!function(e){if(null===e.parentNode)return!1;e.parentNode.removeChild(e)}(n)};return r(e),function(t){if(t){if(t.css===e.css&&t.media===e.media&&t.sourceMap===e.sourceMap)return;r(e=t)}else i()}}e.exports=function(e,t){(t=t||{}).singleton||"boolean"==typeof t.singleton||(t.singleton=(void 0===r&&(r=Boolean(window&&document&&document.all&&!window.atob)),r));var n=c(e=e||[],t);return function(e){if(e=e||[],"[object Array]"===Object.prototype.toString.call(e)){for(var r=0;r<n.length;r++){var i=o(n[r]);a[i].references--}for(var s=c(e,t),l=0;l<n.length;l++){var u=o(n[l]);0===a[u].references&&(a[u].updater(),a.splice(u,1))}n=s}}}},229:(e,t,n)=>{"use strict";var r=n(379),i=n.n(r),a=n(890);i()(a.Z,{insert:"head",singleton:!1}),a.Z.locals;const o=JSON.parse('{"photographers":[{"name":"Mimi Keel","id":243,"city":"London","country":"UK","tags":["portrait","events","travel","animals"],"tagline":"Voir le beau dans le quotidien","price":400,"portrait":"MimiKeel.jpg"},{"name":"Ellie-Rose Wilkens","id":930,"city":"Paris","country":"France","tags":["sports","architecture"],"tagline":"Capturer des compositions complexes","price":250,"portrait":"EllieRoseWilkens.jpg"},{"name":"Tracy Galindo","id":82,"city":"Montreal","country":"Canada","tags":["art","fashion","events"],"tagline":"Photographe freelance","price":500,"portrait":"TracyGalindo.jpg"},{"name":"Nabeel Bradford","id":527,"city":"Mexico City","country":"Mexico","tags":["travel","portrait"],"tagline":"Toujours aller de l\'avant","price":350,"portrait":"NabeelBradford.jpg"},{"name":"Rhode Dubois","id":925,"city":"Barcelona","country":"Spain","tags":["sport","fashion","events","animals"],"tagline":"Je crée des souvenirs","price":275,"portrait":"RhodeDubois.jpg"},{"name":"Marcel Nikolic","id":195,"city":"Berlin","country":"Germany","tags":["travel","architecture"],"tagline":"Toujours à la recherche de LA photo","price":300,"portrait":"MarcelNikolic.jpg"}]}');console.log(o)}},e=>{"use strict";e(e.s=229)}]);
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkfisheye"] = self["webpackChunkfisheye"] || []).push([["index"],{
+
+/***/ "./src/data/data.json":
+/*!****************************!*\
+  !*** ./src/data/data.json ***!
+  \****************************/
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = JSON.parse('{\"photographers\":[{\"name\":\"Mimi Keel\",\"id\":243,\"city\":\"London\",\"country\":\"UK\",\"tags\":[\"portrait\",\"events\",\"travel\",\"animals\"],\"tagline\":\"Voir le beau dans le quotidien\",\"price\":400,\"portrait\":\"MimiKeel.jpg\"},{\"name\":\"Ellie-Rose Wilkens\",\"id\":930,\"city\":\"Paris\",\"country\":\"France\",\"tags\":[\"sports\",\"architecture\"],\"tagline\":\"Capturer des compositions complexes\",\"price\":250,\"portrait\":\"EllieRoseWilkens.jpg\"},{\"name\":\"Tracy Galindo\",\"id\":82,\"city\":\"Montreal\",\"country\":\"Canada\",\"tags\":[\"art\",\"fashion\",\"events\"],\"tagline\":\"Photographe freelance\",\"price\":500,\"portrait\":\"TracyGalindo.jpg\"},{\"name\":\"Nabeel Bradford\",\"id\":527,\"city\":\"Mexico City\",\"country\":\"Mexico\",\"tags\":[\"travel\",\"portrait\"],\"tagline\":\"Toujours aller de l\\'avant\",\"price\":350,\"portrait\":\"NabeelBradford.jpg\"},{\"name\":\"Rhode Dubois\",\"id\":925,\"city\":\"Barcelona\",\"country\":\"Spain\",\"tags\":[\"sport\",\"fashion\",\"events\",\"animals\"],\"tagline\":\"Je crée des souvenirs\",\"price\":275,\"portrait\":\"RhodeDubois.jpg\"},{\"name\":\"Marcel Nikolic\",\"id\":195,\"city\":\"Berlin\",\"country\":\"Germany\",\"tags\":[\"travel\",\"architecture\"],\"tagline\":\"Toujours à la recherche de LA photo\",\"price\":300,\"portrait\":\"MarcelNikolic.jpg\"}]}');\n\n//# sourceURL=webpack://fisheye/./src/data/data.json?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/style.css */ \"./src/styles/style.css\");\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_style_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _data_data_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./data/data.json */ \"./src/data/data.json\");\n/* harmony import */ var _test_chunk_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./test-chunk.js */ \"./src/test-chunk.js\");\n/* harmony import */ var _test_chunk_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_test_chunk_js__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\nconsole.log(_data_data_json__WEBPACK_IMPORTED_MODULE_1__);\n\n//# sourceURL=webpack://fisheye/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/styles/style.css":
+/*!******************************!*\
+  !*** ./src/styles/style.css ***!
+  \******************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:3)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> h1 {\\n|   \\n|     color: green;\");\n\n//# sourceURL=webpack://fisheye/./src/styles/style.css?");
+
+/***/ }),
+
+/***/ "./src/test-chunk.js":
+/*!***************************!*\
+  !*** ./src/test-chunk.js ***!
+  \***************************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack://fisheye/./src/test-chunk.js?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
