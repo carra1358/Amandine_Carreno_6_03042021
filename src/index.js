e8f011534cf88df52a657622d6a862889e9c6a90
carra1358 <amandinecarreno13@gmail.com>
2021-04-05 19:38:02 +0200

    index style completed
    - mobile version
    -small mobile version
    -desktop version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import "../styles/stylesheet.scss";
+import "../styles/sass/responsive.scss";
 import photographers from "./data/data.json";
 const logo =  require("../images/logo.png");
 const mimi = require("../images/Mimi/Portrait_Nora.jpg");
+const ellie = require("../images/Ellie Rose/Architecture_Connected_Curves.jpg");
+const tracy = require("../images/Tracy/Fashion_Urban_Jungle.jpg");
+const nabeel = require("../images/Nabeel/Travel_Outdoor_Baths.jpg");
+const rhode = require("../images/Rhode/Fashion_Melody_Red_on_Stripes.jpg");
+const marcel = require("../images/Marcel/Travel_Tower.jpg");
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 
