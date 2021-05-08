6ee3c1045f66ea9027e414b5e68b66c59f2b504a
amandine carreno <30933638+carra1358@users.noreply.github.com>
2021-05-08 18:06:04 +0200

    add modal and fix Media path
    -ajout formulaire maquette téléphone
    -correction affichage: erreur json ou nom de dossier des media

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-console.log(config.autoA11y) // true
+import { config } from "@fortawesome/fontawesome-svg-core";
+console.log(config.autoA11y); // true
 import "../styles/stylesheet.scss";
 import "../styles/sass/responsive.scss";
 const logo = require("../images/logo.png");
@@ -7,13 +7,14 @@ const logo = require("../images/logo.png");
 const getData = () => import("./data/data.json");
 const template = document.getElementById("nos_photographes");
 
-
-getData().then(data => {data.default.photographers.map(element => { 
-
-return  template.innerHTML +=`
+getData().then((data) => {
+  data.default.photographers.map((element) => {
+    return (template.innerHTML += `
 <li class="card">
-<a href="photographes.html?id=${element.id}">
-<div class="profil-img"><img src="${"../images/PhotographersIDPhotos/"+ element.portrait}" alt=""> </div> 
+<a href="photographes.html?id=${element.id}&name=${element.name.split(" ")[0]}">
+<div class="profil-img"><img src="${
+      "../images/PhotographersIDPhotos/" + element.portrait
+    }" alt=""> </div> 
 <h2>${element.name}</h2>
 </a>
 <p>
@@ -22,12 +23,15 @@ return  template.innerHTML +=`
     <span class="prix">${element.price}€/jour</span><br>
 </p>
 <div class="card-tagwrapper">
-   ${element.tags.map(tag =>`<a href="" class="tag"><span class="screen-reader">tag</span><i class="fas fa-hashtag" aria-hidden="true"></i>${tag}</a>`).join("")}
+   ${element.tags
+     .map(
+       (tag) =>
+         `<a href="" class="tag"><span class="screen-reader">tag</span><i class="fas fa-hashtag" aria-hidden="true"></i>${tag}</a>`
+     )
+     .join("")}
 </div>
 </li>
 
-`
-    }
-  
-)});
-
+`);
+  });
+});
--- src/photographes.js
+++ src/photographes.js
@@ -1,157 +1,140 @@
 import "../styles/stylesheet.scss";
 import "../styles/sass/responsive_photographer.scss";
-import { config } from '@fortawesome/fontawesome-svg-core'
-console.log(config.autoA11y) // true
+import { config } from "@fortawesome/fontawesome-svg-core";
+console.log(config.autoA11y); // true
 
 //DOM
 // conteneur template
 const photographerProfil = document.getElementById("photographer-profil");
 const galerieMedia = document.getElementById("galerie-media");
-const photographerTag = document.getElementById("photographer-tag")
+const photographerTag = document.getElementById("photographer-tag");
+const modalPhotographerName = document.getElementById(
+  "modal-photographer-name"
+);
 //Url params
 const querystring = window.location.search;
 const urlParams = new URLSearchParams(querystring);
 const getIdParams = urlParams.get("id");
+const getNameParams = urlParams.get("name");
 
 //importation du json
 const getData = () => import("./data/data.json");
 
-getData().then(data => {
-   const photographer =  data.default.photographers.find(p => p.id == getIdParams);
-   const media = data.default.media.filter( m => m.photographerId == photographer.id && m.image != undefined );
-   if (photographer === undefined){
-       window.location.href = "index.html"
-   }
-   renderProfilInfo(photographer);
-   renderTag(photographer);
-   renderMedia(photographer, media);
-   
-   
-});
+getData().then((data) => {
+  const photographer = data.default.photographers.find(
+    (p) => p.id == getIdParams
+  );
+  const media = data.default.media
+    .filter((m) => m.photographerId == photographer.id)
+    .map((m) => factory(m))
+    .join(" ");
+  if (photographer === undefined) {
+    window.location.href = "index.html";
+  }
 
+  renderProfilInfo(photographer);
+  renderTag(photographer);
+  console.log(media);
+  galerieMedia.innerHTML = media;
+  modalPhotographerName.innerHTML = `${photographer.name}`;
+
+  // renderMedia(photographer, media);
+});
 
 function renderProfilInfo(photographer) {
   photographerProfil.innerHTML = `
         <div id="profil-text">
         <h1 id="profil-header">${photographer.name}</h1>
         <p>
-            <span class="ville">${photographer.city}, ${photographer.country}</span><br>
+            <span class="ville">${photographer.city}, ${
+    photographer.country
+  }</span><br>
             <span class="quote">${photographer.tagline}</span><br>
             <span class="prix">${photographer.price}€/jour</span><br>
         </p>
        </div>
-        <div class="profil-img"><img src="${"../images/PhotographersIDPhotos/"+ photographer.portrait}" alt=""> </div> 
+        <div class="profil-img"><img id="profil-img-photographes" src="${
+          "../images/PhotographersIDPhotos/" + photographer.portrait
+        }" alt=""> </div> 
          
-        `
-};
+        `;
+}
 
 function renderTag(photographer) {
-    photographerTag.innerHTML = `
-    <div class="card-tagwrapper">
-    ${photographer.tags.map(tag =>`<a href="" class="tag"><span class="screen-reader">tag</span><i class="fas fa-hashtag" aria-hidden="true"></i>${tag}</a>`).join("")}
-         </div>
-    `
-};
-
-function renderMedia(photographer, media) {
+  photographerTag.innerHTML = `
     
-galerieMedia.innerHTML += media.map( m => 
-  
-    `<figure class="media">
-    <figcaption class="media-info">
-        <span>${m.image.split(/[._]/).slice(1,-1).join(" ")}</span>
-        <span>${m.price}€</span>
-        <span>${m.likes}<i class="fas fa-heart"></i></span>
-    </figcaption>
-    </figure>`).join("")                      
-};
-
-  class Media {
-        constructor(id,photographerId,tags,likes,date,price){
-         id;
-         photographerId;
-         tags;
-         likes;
-         date;
-         price;
+    ${photographer.tags
+      .map(
+        (tag) =>
+          `<a href="" class="tag"><span class="screen-reader">tag</span><i class="fas fa-hashtag" aria-hidden="true"></i>${tag}</a>`
+      )
+      .join("")}
          
-           }
-      createMedia () {
-       console.log("cest un media")}
-    }
- 
-  
+    `;
+}
+
+class Media {
+  constructor(id, photographerId, tags, likes, date, price, src, name) {
+    this.id = id;
+    this.photographerId = photographerId;
+    this.tags = tags;
+    this.likes = likes;
+    this.date = date;
+    this.price = price;
+    this.src = src;
+    this.name = name;
+  }
+  createMediaContent() {}
+}
+
 class Video extends Media {
-    constructor(id,photographerId,tags,likes,date,price,video){
-       super(id,photographerId,tags,likes,date,price)
-       this.video = video
-    }
-    createMedia(){
-        console.log("ce media est une video")
-        //galerieMedia.appendChild(document.createElement("video")).innerHTML =`
-       // src="../images/${photographer.name.split(" ")[0]}/${m.video}" alt="" `
-    }
-    
+  createMediaContent() {
+    return `<figure class="media">
+    <video src="../images/${this.name}/${this.src}"></video>
+    <figcaption class="media-info">
+        <span>${this.src.split(/[._]/).slice(1, -1).join(" ")}</span>
+        <span>${this.price}€</span>
+        <span>${this.likes}<i class="fas fa-heart"></i></span>
+    </figcaption>
+    </figure>`;
+  }
 }
 
 class Image extends Media {
-    constructor(id,photographerId,tags,likes,date,price,image){
-    super(id,photographerId,tags,likes,date,price)
-       this.image = image
-       
-    }
-
-    createMedia(){
-        return galerieMedia.innerHTML += `<p>CEST UNE IMAGE ${this.id}</p>`
-    }
-
+  createMediaContent() {
+    return `<figure class="media">
+    <img src="../images/${this.name}/${this.src}" alt="">
+    <figcaption class="media-info">
+        <span>${this.src.split(/[._]/).slice(1, -1).join(" ")}</span>
+        <span>${this.price}€</span>
+        <span>${this.likes}<i class="fas fa-heart"></i></span>
+    </figcaption>
+    </figure>`;
+  }
 }
- getData().then( data => { 
-     
-     const media = data.default.media;
-     let image = new Image();
-         image = media.from(Image);
-     console.log(image);
-     let video = new Video();
-     
-      media.map(m =>  {
-      
-         function factory (m){
 
-        function Isimage(){
-             if(m.hasOwnProperty("image")){
-            return image.createMedia()}
-        }
-        function IsVideo(){
-            if (m.hasOwnProperty("video")){
-        return video.createMedia()
-    }
-        }
-       Isimage();
-       IsVideo();
-        
-    
-    };
-    console.log(factory(m));
-     });
-  
-    });
-
-    console.log( new Media());
-    
-
-
-let image1 = new Image(12,13,"mimi",12,"29 janvier", "30$", "photo.jpg");
-let video2 = new Video(13,16,"jojo",17,"20 janvier", "60$", "phoo.mp4");
-
-
-
-
-
-
-
-
-
-
-
-
+function factory(media) {
+  if (media.hasOwnProperty("image")) {
+    return new Image(
+      media.id,
+      media.photographerId,
+      media.tags,
+      media.likes,
+      media.date,
+      media.price,
+      media.image,
+      getNameParams
+    ).createMediaContent();
+  } else if (media.hasOwnProperty("video")) {
+    return new Video(
+      media.id,
+      media.photographerId,
+      media.tags,
+      media.likes,
+      media.date,
+      media.price,
+      media.video,
+      getNameParams
+    ).createMediaContent();
+  }
+}
